ded93497a81b6431aecf6083874dd4797328450f
SumitSharma2210 <sumit.sharma.gkv@gmail.com>
2025-01-15 10:18:58 +0530

    latest commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const apiUrl = "https://event-management-backend-51us.onrender.com";
 
 console.log("API URL being used:", apiUrl);
 
